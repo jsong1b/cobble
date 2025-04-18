f6a6262559af58d9965d8eb6cf65aa28237617d3
jsong1b <141759463+jsong1b@users.noreply.github.com>
2025-04-17 18:48:41 -0700

    Finish implementing the function to extract blocks

--- bootstrap/bootstrap.py
+++ bootstrap/bootstrap.py
@@ -13,11 +13,23 @@ def main():
         print('Not enough arguments')
         sys.exit(1)
 
+    blocks = []
     for arg in sys.argv[1:]:
-        (err, blocks) = extract_blocks(arg)
+        (err, file_blocks) = extract_blocks(arg)
         if err != None:
             print(f'Could not extract blocks from file "{arg}": {err}', file = sys.stderr)
             continue
+        blocks += [file_blocks]
+
+    print(blocks)
+
+
+class source_block:
+    def __init__(self, origin = '', export = None, name = '', lines = []):
+        self.origin = origin
+        self.export = export
+        self.name = name
+        self.lines = lines
 
 
 def extract_blocks(file_path):
@@ -29,37 +41,41 @@ def extract_blocks(file_path):
         return(str(e), None)
 
     blocks = []
+    cur_block = source_block(origin = file_path)
     parser_state = None
     for line in file_lines:
-        # print(parser_state, line)
-
         if ((parser_state == None and not re.match(r'^((`.+`)|\[.+\]\(.+\)):$', line)) or
             (parser_state == 'Named' and not re.match(r'^```.*$', line))):
             continue
 
         if parser_state == None and re.match(r'^((`.+`)|\[.+\]\(.+\)):$', line):
-            # naming the block
+            if re.match(r'^`.+`:$', line):
+                cur_block.name = line[1:-2]
+            else:
+                match = line.split('](')
+                cur_block.export = match[0][1:]
+                cur_block.name = match[1][:-2]
+
             parser_state = 'Named'
-            print(line)
             continue
 
         if parser_state == 'Named' and re.match(r'^```.*$', line):
-            # at start of block
             parser_state = 'In Block'
             continue
 
         if parser_state == 'In Block' and re.match(r'^```$', line):
-            # save block
+            blocks += [cur_block]
+            cur_block = source_block(origin = file_path)
             parser_state = None
             continue
 
         if not parser_state == 'In Block':
             continue
 
-        print(line)
+        cur_block.lines += [line]
 
 
-    return (None, None)
+    return (None, blocks)
 
 
 if __name__ == '__main__':
